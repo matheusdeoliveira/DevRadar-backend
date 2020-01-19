da0b5bb0ee1d0cc5a51b49646788f89735242fdd
José Matheus <jose.matheus_ext@michelin.com>
2020-01-19 15:04:31 +0000

    Finalizando projeto

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
+const http = require('http');
+const { setWebSocket } = require( './websocket' )
+
 const app = express();
+const server = http.Server(app);
+
+setWebSocket( server )
 
 mongoose.connect('mongodb+srv://omnistack2020:badmdado@cluster0-86akr.mongodb.net/week10?retryWrites=true&w=majority',{
 
